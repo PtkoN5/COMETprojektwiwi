cbc94d556e1996df5d7a1180406f1f8a606ba001
Mika <49035141+PtkoN5@users.noreply.github.com>
2025-01-20 11:13:41 +0100

    hinzufügen von Packages

--- Abgabe 2/main.r
+++ Abgabe 2/main.r
@@ -1,9 +1,12 @@
 ### plotly pakete
-### r markdown vielleicht auch benutzen
+###install.packages("rmarkdown")    ### Muss für Dokumentation überlegt werden
+###install.packages("testthat")     ### Muss für 3.4 benutzt werden
+###install.packages("ggplot2")      ### Muss für 4.2/4.4 benutzt werden
+###install.packages("plotly")       ### Muss Benutz werden für 4.6
 
 ###3.1
 ###3.2
-###3.3
+###3.3 
 ###3.4
 ###3.5
 
