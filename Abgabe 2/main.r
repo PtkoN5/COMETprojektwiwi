b0931fa1a82d439faee7d23a472dbbaeb758837b
Mika <49035141+PtkoN5@users.noreply.github.com>
2025-01-20 15:51:27 +0100

    Update main.r

--- Abgabe 2/main.r
+++ Abgabe 2/main.r
@@ -1,18 +1,18 @@
-### R Packages die wir brauchen für Abgabe 2
-###install.packages("rmarkdown")    ### Muss für Dokumentation überlegt werden
-###install.packages("testthat")     ### Muss für 3.4 benutzt werden
-###install.packages("ggplot2")      ### Muss für 4.2/4.4 benutzt werden
-###install.packages("plotly")       ### Muss Benutz werden für 4.6
+# R Packages die wir brauchen für Abgabe 2
+#install.packages("rmarkdown")    # Muss für Dokumentation überlegt werden
+#install.packages("testthat")     # Muss für 3.4 benutzt werden
+#install.packages("ggplot2")      # Muss für 4.2/4.4 benutzt werden
+#install.packages("plotly")       # Muss Benutz werden für 4.6
 
-###3.1
-###3.2
-###3.3 
-###3.4
-###3.5
+#3.1
+#3.2
+#3.3 
+#3.4
+#3.5
 
-###4.1
-###4.2
-###4.3
-###4.4
-###4.5
-###4.6
+#4.1
+#4.2
+#4.3
+#4.4
+#4.5
+#4.6
